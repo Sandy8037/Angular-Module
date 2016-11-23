ab6408d97294aaf6f60b6311c5937c003a9780f5
Sandeep Devarapalli <Sandeep Devarapalli>
2016-11-23 11:00:10 -0800

    Angular ui-router

---
+++ examples/routing/src/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use scrict';
+
+angular.module('RoutingApp', ['ui.router']);
+
+angular.module('RoutingApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to tab 1 if no other URL
+  $urlRouterProvider.otherwise('/tab1');
+
+  // UI states
+  $stateProvider
+  .state('tab1', {
+    url: '/tab1',
+    templateUrl: 'src/tab1.html'
+  })
+  .state('tab2', {
+    url:'/tab2',
+    templateUrl: 'src/tab2.html'
+  });
+}
+
+})();
